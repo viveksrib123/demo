ccbb04e3d5df4c943b510cf0baa35a691cf9ec7d
viveksrib123 <vivekpandey002@gmail.com>
2020-07-22 00:06:53 +0530

    Update to fetch config from git
    Update to fetch config from git

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,29 +1,48 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.discovery.EurekaClient;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
-@RestController
+@EnableFeignClients
+@Controller
+@Configuration
 public class DemoApplication {
 
 	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
-	@RequestMapping("/myapp")
-	public String home() {
-		logger.info("Demo app root looger: response 200");
-		return "Hello Docker World";
-	}
+	@Autowired
+    private GreetingClient greetingClient;
 	
-	@RequestMapping("/produceerror")
-	public String produceerror() {
-		logger.error("Demo app root looger: response 500");
-		return "Error response";
-	}
+	@Value("${environment}")
+	String environment;
+	
+	@RequestMapping("/")
+    public String hello(Model model) {
+		model.addAttribute("environment", environment);
+        return "hello";
+    }
+	
+	@RequestMapping("/get-greeting")
+    public String greeting(Model model) {
+        model.addAttribute("greeting", greetingClient.greeting());
+        return "greeting-view";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
---
+++ src/main/java/com/example/demo/GreetingClient.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("backend")
+public interface GreetingClient {
+    @RequestMapping("/greeting")
+    String greeting();
+}
